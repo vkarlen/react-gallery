b214fb03a8980d1af863533040b4f33cc1fe520f
Vada Karlen <vada.karlen@gmail.com>
2021-02-19 20:08:00 -0600

    Added first batch of pictures. Hope you like dogs

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,34 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: 'images/frida-now.jpg',
+    description: 'My dogs Frida & Ender.',
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: 'images/frida-baby.jpg',
+    description: 'Frida as a baby.',
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: 'images/frida-first.jpg',
+    description: 'The first time Frida and Ender met.',
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: 'images/ender-hiking.jpg',
+    description: 'Hiking with Ender last summer in Tettegouche.',
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: 'images/end-first.jpg',
+    description: 'The first day Ender came home.',
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
